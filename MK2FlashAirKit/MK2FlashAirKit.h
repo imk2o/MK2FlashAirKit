40f6c594846e8200636f430a16f05d076cf91fb5
imk2o <imk2o@icloud.com>
2016-07-01 19:43:58 +0900

    Initial Commit

---
+++ MK2FlashAirKit/MK2FlashAirKit.h
@@ -0,0 +1,19 @@
+//
+//  MK2FlashAirKit.h
+//  MK2FlashAirKit
+//
+//  Created by k2o on 2016/07/01.
+//  Copyright © 2016年 Yuichi Kobayashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MK2FlashAirKit.
+FOUNDATION_EXPORT double MK2FlashAirKitVersionNumber;
+
+//! Project version string for MK2FlashAirKit.
+FOUNDATION_EXPORT const unsigned char MK2FlashAirKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MK2FlashAirKit/PublicHeader.h>
+
+
